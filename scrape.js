71ec9c033184ac37c5d29264e1cdfad8d4df8921
General Orange <erik.17021995@gmail.com>
2019-11-05 23:31:50 +0100

    Write to games.csv file

--- scrape.js
+++ scrape.js
@@ -1,6 +1,10 @@
 const request = require('request');
-
 const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('games.csv');
+
+// Write Headers
+writeStream.write(`Title, Merchant, Price \n`);
 
 request('https://www.allkeyshop.com/blog/', (error, response, html) => {
     if(!error && response.statusCode == 200) {
@@ -8,10 +12,13 @@ request('https://www.allkeyshop.com/blog/', (error, response, html) => {
 
         $('#Top25 .topclick-list-element').each((i, el) => {
             const item = $(el).find('.topclick-list-element-game .topclick-list-element-game-title').text();
+            const merchant = $(el).find('.topclick-list-element-game .topclick-list-element-game-merchant').text();
             const price = $(el).find('.topclick-list-element-price').text();
-            console.log(item, '|', price);
+            
+            // Write row to csv
+            writeStream.write(`${item}, ${merchant}, ${price} \n`);
         });
 
-        
+        console.log('Scraping done...');
     }
 });
