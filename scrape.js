6fb3d479e023d42df9f9f265e07451d7dfd17140
General Orange <erik.17021995@gmail.com>
2019-11-09 16:55:03 +0100

    Changes file to .json for better inheritence

--- scrape.js
+++ scrape.js
@@ -1,23 +1,36 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const writeStream = fs.createWriteStream('games.csv');
+const writeStream = fs.createWriteStream('games.json');
 
-// Write Headers
-writeStream.write(`Title, Merchant, Price \n`);
 
 request('https://www.allkeyshop.com/blog/', (error, response, html) => {
     if(!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
 
+        let games = {
+            games: [
+        ]};
+
         $('#Top25 .topclick-list-element').each((i, el) => {
+            
             const item = $(el).find('.topclick-list-element-game .topclick-list-element-game-title').text();
             const merchant = $(el).find('.topclick-list-element-game .topclick-list-element-game-merchant').text();
             const price = $(el).find('.topclick-list-element-price').text();
+
+            const game = {
+                title: item,
+                merchant: merchant,
+                price: price
+            }
+
+            games.games.push(game);
             
-            // Write row to csv
-            writeStream.write(`${item}, ${merchant}, ${price} \n`);
+            
         });
+
+        // Write row to csv
+        writeStream.write(JSON.stringify(games));
 
         console.log('Scraping done...');
     }
